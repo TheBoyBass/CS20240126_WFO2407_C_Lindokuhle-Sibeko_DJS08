238ca8342e50d38a1faf5ba3f9031009a7bc3dda
Lindokuhle Sibeko <138812996+TheBoyBass@users.noreply.github.com>
2024-11-19 08:15:58 +0200

    "Updated Navbar component to use header element and added site-logo class to home link"

--- components/Navbav.jsx
+++ components/Navbav.jsx
@@ -2,11 +2,13 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-    </nav>
-  );
+    <header>
+        <Link className="site-logo" to="/">#VanLife</Link>
+        <nav>
+        <Link to="/about">About</Link>
+        </nav>
+    </header>
+    );
 }
 
 export default Navbar;
